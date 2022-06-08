0c472f4770a9e01b920998556d2260bbc4b26500
Anthony Petrov <beavean@gmail.com>
2022-06-08 19:00:12 +0300

    Added labels variables

--- 7 Swift Words/ViewController.swift
+++ 7 Swift Words/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+
+    }
 
 }
 
